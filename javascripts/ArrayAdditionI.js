c47e69354caf164d5f7581cbc49fc693f15c7310
Dan Mee <danmee10@gmail.com>
2015-07-20 18:52:02 -0600

    ArrayAdditionI

---
+++ javascripts/ArrayAdditionI.js
@@ -0,0 +1,34 @@
+$(function(){
+  $("button#run-code").click(function(e){
+    e.preventDefault;
+    $("#output").empty();
+    var input = $("#input").val().split(",");
+    var output = ArrayAdditionI(input);
+
+
+    function ArrayAdditionI(arr) {
+      var largestNum = arr.sort(function(a, b){ return b - a;}).splice(0, 1)[0];
+      var combs = combinations(arr);
+
+      var nonePass = combs.every(function(nums){
+        return nums.reduce(function(a, b){ return a + b; }) !== largestNum;
+      });
+
+      return !nonePass;
+
+    }
+    function combinations(set) {
+      return (function acc(xs, set) {
+        var x = xs[0];
+        if(typeof x === "undefined")
+          return set;
+        for(var i = 0, l = set.length; i < l; ++i)
+          set.push(set[i].concat(x));
+        return acc(xs.slice(1), set);
+      })(set, [[]]).slice(1);
+    };
+
+
+    $("#output").append(output);
+  })
+});
